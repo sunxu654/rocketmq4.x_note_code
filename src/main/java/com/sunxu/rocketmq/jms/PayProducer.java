b4e0ea9e6e554dd4f47ff867c6c6c317ae50aeef
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-06-21 15:16:33 +0800

    创建producer

--- src/main/java/com/sunxu/rocketmq/jms/PayProducer.java
+++ src/main/java/com/sunxu/rocketmq/jms/PayProducer.java
@@ -1,10 +1,39 @@
 package com.sunxu.rocketmq.jms;
 
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PayProducer {
-    private String poducerGroup = "pay_group";
+    /**
+     * producer的组和名称服务器
+     */
+    private String producerGroup = "pay_group";
     private String nameServerAddr = "localhost:9876";
+
+    private DefaultMQProducer producer;
+
+    public PayProducer() {
+        producer = new DefaultMQProducer(producerGroup);
+
+        /**
+         * 可以设置多个nameserver,形式:"<ip>,<ip>,<ip>"
+         */
+        producer.setNamesrvAddr(nameServerAddr);
+
+        start();
+    }
+
+    /**
+     * TODO 把start放在外面的原因是
+     */
+    public void start() {
+        try {
+            this.producer.start();
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
