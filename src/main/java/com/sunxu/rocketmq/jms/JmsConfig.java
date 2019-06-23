dc646790d109f73c497114a3196c11cb9b79dc59
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-06-23 10:19:31 +0800

    增加一主一从,异步复制broker节点和双nameserver

--- src/main/java/com/sunxu/rocketmq/jms/JmsConfig.java
+++ src/main/java/com/sunxu/rocketmq/jms/JmsConfig.java
@@ -5,7 +5,10 @@ package com.sunxu.rocketmq.jms;
  *
  */
 public class JmsConfig {
-    public static final String NAME_SERVER = "192.168.245.4:9876";
+    /**
+     * 增加一主一从,异步复制broker节点和双nameserver
+     */
+    public static final String NAME_SERVER = "192.168.245.4:9876,192.168.245.5:9876";
     public static final String TOPIC = "dclass_pay_test_topic";
 
 }
