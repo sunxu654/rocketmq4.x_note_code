19deaa505cfa29400048a13e89e07af9f2f1666a
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-06-23 10:49:01 +0800

    增加必备知识点

--- src/main/java/com/sunxu/rocketmq/jms/JmsConfig.java
+++ src/main/java/com/sunxu/rocketmq/jms/JmsConfig.java
@@ -19,4 +19,22 @@ public class JmsConfig {
     public static final String NAME_SERVER = "192.168.245.4:9876,192.168.245.5:9876";
     public static final String TOPIC = "dclass_pay_test_topic";
 
+    /**
+     * 讲解RocketMQ主从同步必备知识点
+     * Broker分为master与slave，一个master可以对应多个Slave，但一个slave只能对应一个master，master与slave通过相同的Broker Name来匹配，不同的broker
+     * ld来定义是master还是slave
+     *
+     * Broker向所有的NameServer结点建立长连接，定时注册Topic和发送元数据信息
+     *
+     * NameServer定时扫描（默认2分钟）所有存活broker的连接，如果超过时间没响应则断开连接（心跳检测），但是consumer客户端不能感知，consumer定时
+     * （30s）从NameServer获取topic的最新信息，所以broker不可用时，consumer最多最需要30s才能发现（Producer的机制一样，在未发现broker宕机前
+     * 发送的消息会失败）
+     *
+     * 只有master才能进行写入操作，slave不允许写入只能同步，同步策略取决于master的配置。
+     *
+     * 客户端消费可以从master和slave消费，默认消费者都从master消费，如果在master挂后，客户端从NameServer中感知到Broker宕机，就会从slave消费，
+     * 感知非实时，存在一定的滞后性，slave不能保证master的消息100%都同步过来了，会有少量的消息丢失。但一旦master恢复，未同步过去的消息会被最终消费掉
+     *
+     * 如果consumer实例的数量比message queue的总数量还多的话，多出来的consumer实例将无法分到queue，也就无法消费到消息，也就无法起到分摊负载的作用，所以需要控制让queue的总数量大于等于consumer的数量
+     */
 }
