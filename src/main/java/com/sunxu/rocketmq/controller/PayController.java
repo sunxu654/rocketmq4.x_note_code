a7bc0fb2026a49ae5d5e3515b5a72cdbce735649
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-06-23 21:51:51 +0800

    增加三种发送消息的方式(同步,异步,OneWay)

--- src/main/java/com/sunxu/rocketmq/controller/PayController.java
+++ src/main/java/com/sunxu/rocketmq/controller/PayController.java
@@ -28,6 +28,7 @@ public class PayController {
          * 也有其他的构造函数,传不同的参数
          */
         Message message = new Message(JmsConfig.TOPIC, "taga","123",("hello rocketmq = " + text).getBytes());
+//       发送方式 1 : 同步发送
 //        /**
 //         * 把message通过producer发送出去
 //         * 发送到消息队列 等待消费者消费
@@ -35,9 +36,12 @@ public class PayController {
 //        SendResult sendResult = payProducer.getProducer().send(message);
 //        System.out.println(sendResult);
 
+//        发送方式2 :异步发送
         /**
+         *
          * 异步发送消息和回调
-         * 在发送消息的同时,开一个线程来反馈发送结果
+         * 在发送消息的同时,开一个线程来做一些对时间要求比较敏感的业务逻辑
+         * 比如用户注册成功后,直接在本地的onsuccess里面调用发放优惠券服务,而不需要用户的消息传输完成再发送
          */
         payProducer.getProducer().send(message, new SendCallback() {
             @Override
@@ -51,6 +55,13 @@ public class PayController {
                 //TODO 补偿机制,比如重新发送
             }
         });
+
+//        发送方式3: send one way
+        /**
+         * 用于给logserver等不需要响应,对性能要求比较高,对可靠性要求比较低的服务 发送请求
+         * 返回值为空,ublic void sendOneway(Message msg)
+         */
+        payProducer.getProducer().sendOneway(message);
         return new HashMap<>();
     }
 
